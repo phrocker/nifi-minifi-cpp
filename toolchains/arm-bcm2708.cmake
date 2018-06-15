8e6f7a6ba76173a6e6f7d0ed506dde433b61f9c9
Marc Parisi <phrocker@apache.org>
2018-06-15 08:53:52 -0400

    comm

--- toolchains/arm-bcm2708.cmake
+++ toolchains/arm-bcm2708.cmake
@@ -30,7 +30,7 @@ SET(CMAKE_SYSTEM_PROCESSOR arm)
 #add_definitions("-mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -marm")
 
 # search for programs in the build host directories
-#SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 #SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
