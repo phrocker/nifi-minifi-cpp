dea37b232705cebefaca850e9094f59c0de49e0d
Marc Parisi <phrocker@apache.org>
2018-08-14 15:03:59 -0400

    windows update

--- libminifi/include/utils/file/FileUtils.h
+++ libminifi/include/utils/file/FileUtils.h
@@ -27,12 +27,13 @@
 #else
 #include <cstring>
 #include <cstdlib>
-#ifndef WIN32
+#ifdef WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <Windows.h>
 #pragma comment(lib, "Ws2_32.lib")
+#else
 #include <sys/stat.h>
 #include <dirent.h>
 #endif
