379a2e5ea275568886e00dc783c3d472508cf434
Marc Parisi <phrocker@apache.org>
2019-04-17 16:10:06 -0400

    sp

---
+++ libminifi/include/processors/ProcessorUtils.h
@@ -0,0 +1,20 @@
+#include <memory>
+#include <vector>
+#include <core/ConfigurableComponent.h>
+#include <core/Processor.h>
+
+namespace org {
+namespace apache {
+namespace nifi {
+namespace minifi {
+namespace processors {
+
+static bool isProcessor(const std::shared_ptr<core::ConfigurableComponent> &comp);
+
+
+
+} /* namespace processors */
+} /* namespace minifi */
+} /* namespace nifi */
+} /* namespace apache */
+} /* namespace org */
