c1b46b8d37a5b4f6d108dd74b79bee76f4815da1
Marc Parisi <phrocker@apache.org>
2019-04-18 07:40:47 -0400

    MINIFICPP-797: Add test and fix linter error

--- libminifi/test/unit/PutFileTests.cpp
+++ libminifi/test/unit/PutFileTests.cpp
@@ -24,7 +24,6 @@
 #include <set>
 #include <fstream>
 
-
 #include "utils/file/FileUtils.h"
 #include "../TestBase.h"
 #include "processors/LogAttribute.h"
@@ -104,8 +103,7 @@ TEST_CASE("PutFileTest", "[getfileputpfile]") {
   REQUIRE(true == std::ifstream(movedFile.str()).good());
 
   file.open(movedFile.str(), std::ios::in);
-  std::string contents((std::istreambuf_iterator<char>(file)),
-                       std::istreambuf_iterator<char>());
+  std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
   REQUIRE("tempFile" == contents);
   file.close();
   LogTestController::getInstance().reset();
@@ -315,5 +313,98 @@ TEST_CASE("PutFileTestFileExistsReplace", "[getfileputpfile]") {
 TEST_CASE("Test generation of temporary write path", "[putfileTmpWritePath]") {
   auto processor = std::make_shared<org::apache::nifi::minifi::processors::PutFile>("processorname");
   REQUIRE(processor->tmpWritePath("a/b/c", "").substr(1, strlen("a/b/.c")) == "a/b/.c");
+}
+
+class PutFileTest {
+};
+
+/**
+ * Will be using the same directory. Start with five files, 3 are pulled and removed via GetFile,
+ * two remain...with two as the max dest files. This should result in not being able to write the final file.
+ *
+ */
+TEST_CASE("TestMaxPutFile", "[testmax01]") {
+  TestController testController;
+
+  LogTestController::getInstance().setDebug<minifi::processors::GetFile>();
+  LogTestController::getInstance().setDebug<TestPlan>();
+  LogTestController::getInstance().setDebug<minifi::processors::PutFile>();
+  LogTestController::getInstance().setDebug<minifi::processors::PutFile::ReadCallback>();
+  LogTestController::getInstance().setDebug<minifi::processors::LogAttribute>();
+
+  std::shared_ptr<TestPlan> plan = testController.createPlan();
+
+  std::shared_ptr<core::Processor> getfile = plan->addProcessor("GetFile", "getfileCreate2");
+
+  std::shared_ptr<core::Processor> putfile = plan->addProcessor("PutFile", "putfile", core::Relationship("success", "description"), true);
+
+  // note we expect this to hit a failure condition due to how this is being configured
+  plan->addProcessor("LogAttribute", "logattribute", core::Relationship("failure", "description"), true);
+
+  char format[] = "/tmp/gt.XXXXXX";
+  char *dir = testController.createTempDirectory(format);
+  plan->setProperty(getfile, org::apache::nifi::minifi::processors::GetFile::Directory.getName(), dir);
+  plan->setProperty(getfile, org::apache::nifi::minifi::processors::GetFile::BatchSize.getName(), "3");
+  plan->setProperty(putfile, org::apache::nifi::minifi::processors::PutFile::Directory.getName(), dir);
+  plan->setProperty(putfile, org::apache::nifi::minifi::processors::PutFile::MaxDestFiles.getName(), "2");
+
+  testController.runSession(plan, false);
+
+  auto records = plan->getProvenanceRecords();
+  std::shared_ptr<core::FlowFile> record = plan->getCurrentFlowFile();
+  REQUIRE(record == nullptr);
+  REQUIRE(records.size() == 0);
+
+  std::vector<std::string> paths;
+
+  std::fstream file;
+  std::stringstream ss;
+  for (int i = 0; i < 5; i++) {
+    ss.str("");
+    ss << dir << "/" << "tstFile" << i << ".ext";
+    file.open(ss.str(), std::ios::out);
+    file << "tempFile";
+    paths.push_back(ss.str());
+    file.close();
+  }
+  plan->reset();
+
+  testController.runSession(plan, false);
+
+  testController.runSession(plan, false);
+
+  records = plan->getProvenanceRecords();
+  record = plan->getCurrentFlowFile();
+  testController.runSession(plan, false);
+
+  unlink(ss.str().c_str());
+
+  REQUIRE(true == LogTestController::getInstance().contains("key:absolute.path value:" + paths.at(0)));
+  REQUIRE(true == LogTestController::getInstance().contains("Size:8 Offset:0"));
+  REQUIRE(true == LogTestController::getInstance().contains("key:path value:" + std::string(dir)));
+  REQUIRE(true == LogTestController::getInstance().contains("has at least 2 files, which exceeds the configured max number of files"));
+  // verify that the fle was moved
+  REQUIRE(false == std::ifstream(ss.str()).good());
+  std::stringstream movedFile;
+  movedFile << dir << "/" << "tstFile0.ext";
+  REQUIRE(false == std::ifstream(movedFile.str()).good());
+
+  file.close();
+
+  int64_t file_count = 0;
+
+  auto counter = [&file_count](const std::string&, const std::string&) -> bool {
+    file_count++;
+    return true;
+  };
+
+  auto logger = logging::LoggerFactory<PutFileTest>::getLogger();
+
+  utils::file::FileUtils::list_dir(dir, counter, logger, false);
+
+  // ensure we have two files.
+  REQUIRE(2 == file_count);
+
+  LogTestController::getInstance().reset();
 }
 
