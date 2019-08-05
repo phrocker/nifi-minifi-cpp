d0b91ba932810631e6b82eb94b7e5bfefc6413a7
Marc Parisi <phrocker@apache.org>
2019-08-05 01:39:48 +0000

    com

---
+++ libminifi/src/utils/OsUtils.cpp
@@ -0,0 +1,146 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "utils/OsUtils.h"
+#include <iostream>
+#include <map>
+
+#ifdef WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+#include <sddl.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#endif
+namespace org {
+namespace apache {
+namespace nifi {
+namespace minifi {
+namespace utils {
+
+#ifdef WIN32
+std::string OsUtils::resolve_common_identifiers(const std::string &id) {
+	static std::map<std::string, std::string> nameMap;
+	if (nameMap.empty()) {
+		nameMap["S-1-0"] = "Null Authority";
+		nameMap["S-1-0-0"] = "Nobody";
+		nameMap["S-1-1-0"] = "Everyone";
+		nameMap["S-1-2"] = "Local Authority";
+		nameMap["S-1-2-0"] = "Local";
+		nameMap["S-1-2-1"] = "Console Logon";
+		nameMap["S-1-3-0"] = "Creator Owner";
+		nameMap["S-1-3-1"] = "Creator Group";
+	}
+	auto name = nameMap.find(id);
+	if (name != std::end(nameMap)) {
+		return name->second;
+	}
+	return "";
+}
+#endif
+
+std::string  OsUtils::userIdToUsername(const std::string &uid) {
+	std::string name;
+		name = uid;
+		if (!name.empty()) {
+#ifdef WIN32
+			const auto resolved_name = resolve_common_identifiers(name);
+			if (!resolved_name.empty()) {
+				return resolved_name;
+			}
+			// First call to LookupAccountSid to get the buffer sizes.
+			PSID pSidOwner = NULL;
+			if (ConvertStringSidToSidA(name.c_str(), &pSidOwner)) {
+				SID_NAME_USE sidType = SidTypeUnknown;
+				DWORD windowsAccountNameSize = 0, dwwindowsDomainSize = 0;
+				LPTSTR windowsDomain = NULL;
+				LPTSTR windowsAccount = NULL;
+
+				// Reallocate memory for the buffers.
+				LookupAccountSid(NULL /** local computer **/, pSidOwner,
+					windowsAccount,
+					(LPDWORD)&windowsAccountNameSize,
+					windowsDomain,
+					(LPDWORD)&dwwindowsDomainSize,
+					&sidType);
+
+				if (windowsAccountNameSize > 0) {
+					windowsAccount = (LPTSTR)GlobalAlloc(
+						GMEM_FIXED,
+						windowsAccountNameSize);
+
+					if (dwwindowsDomainSize > 0) {
+						windowsDomain = (LPTSTR)GlobalAlloc(
+							GMEM_FIXED,
+							dwwindowsDomainSize);
+					}
+
+					if (LookupAccountSid(
+						NULL,
+						pSidOwner,
+						windowsAccount,
+						(LPDWORD)&windowsAccountNameSize,
+						windowsDomain,
+						(LPDWORD)&dwwindowsDomainSize,
+						&sidType)) {
+						if (dwwindowsDomainSize > 0) {
+							std::string domain = std::string(windowsDomain);
+							name = domain + "\\";
+							name += std::string(windowsAccount);
+						}
+						else {
+							name = std::string(windowsAccount);
+						}
+
+					}
+
+					GlobalFree(windowsAccount);
+					if (dwwindowsDomainSize > 0)
+						GlobalFree(windowsDomain);
+				}
+			}
+#else 
+			static std::mutex mtx;
+			auto ptr = name.c_str();
+			char *end = nullptr; // it will be unused
+			uid_t ret = std::strtol(ptr, &end, 10);
+			if (ret > 0) {
+				// getpwuid is not thread safe. as a result we will control access here
+				std::lock_guard<std::mutex> lock(mtx);
+				struct passwd *pwd = getpwuid(uid);
+				if (nullptr != pwd) {
+					name = pwd->pw_name
+				}
+			}
+			// nothing to perform. noop until linux impl can be created.
+#endif
+		}
+	return name;
+}
+
+} /* namespace utils */
+} /* namespace minifi */
+} /* namespace nifi */
+} /* namespace apache */
+} /* namespace org */
+
